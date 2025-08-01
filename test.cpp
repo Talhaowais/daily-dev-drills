8228d574fa3b4cb16b52b165ba4cfa737a961b52
M.Talha <talhaowais345@gmail.com>
2025-08-01 06:24:56 +0500

    Testing C++ file

---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // variables
+    // float num = 5234.1264;
+    // double num2 = 5234.1264;
+    // // ten digits
+    // int num3 =1234567891;
+    // char letter = 'L';
+    // string name = "Talha";
+    // cout << "Num : \t" << num << endl;
+    // cout << "Num2: \t" << num2 << endl;
+    // cout << "Num3: \t" << num3 << endl;
+    // cout << "letter: \t" << letter << endl;
+    // cout << "name: \t" << name << endl;
+
+    // if(num<=5){
+    //     cout << "Number is less than" << num << "\n";
+    // } 
+    // if(num==5){
+    //     cout << "Number is equal to" << num << endl;
+    // }
+    // else{
+    //     cout << "Number is greater than 5";
+    // }
+
+    int num = 6;
+    // % modulus short form mod return remainder
+    // / divide it returns quotient 
+    if(num%2==0){
+        cout << "Even\n";
+    } else{
+        cout << "Odd\n";
+    }
+
+    return 0;
+}
