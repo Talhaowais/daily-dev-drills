7fd1b6a60bbe35e3df24eb312c121550b9b9c29e
M.Talha <talhaowais345@gmail.com>
2025-08-03 19:47:49 +0500

    lesson 3

---
+++ Assignments/3rd_Assignment.cpp
@@ -0,0 +1,30 @@
+// Q.No.1 Write a program that calculates
+// the sum of even and odd numbers from 1 to N separately
+// Q.No.2 Write a program that prints a square of * of size N.
+// Example N = 3
+// ***
+// ***
+// ***
+// Q.No.3  Use a for loop to print the following pattern for N rows:
+// Example N=4
+// *
+// **
+// ***
+// ****
+// Q.No.4 Write a program that counts how many digits are
+// in a number using a for loop or controlled loop.
+// Example Input: 12345 
+// Output: 5
+
+// num = 123
+// num = num/10 (12)
+// Q.No.5 Write a program using a for loop to 
+// print the first N terms of the Fibonacci series.
+// Example N=5
+// Fibonacci Series
+// 1 1 2 3 5 8 13 21 34 55 
+// Q.No.6 Write a program that prints a square of * of size N.
+// Example N = 3
+// ***
+// * *
+// ***
--- Lessons/loops.cpp
+++ Lessons/loops.cpp
@@ -8,12 +8,38 @@ int main()
     // {
     //     cout << "5 x " << i << " = " << 5*i << endl;
     // }
-    int sum = 0;
-    for (int i = 1; i <= 10; i++)// i++ = i+1
+    // int sum = 0;
+    // for (int i = 1; i <= 10; i++)// i++ = i+1
+    // {
+    //     sum=sum+i;
+    // }
+    // cout << "Sum is " << sum << endl;
+
+    // int num=123;
+    // for(;num>0;){
+    //     cout << num << endl;
+    //     num = num/10;
+    // }
+    // cout << num << endl;
+
+    for (int i = 0; i < 3; i++) // row
     {
-        sum=sum+i;
+        for (int j = 0; j < 3; j++) // column
+        {
+            if(i==0){
+                cout << "1 ";
+            }
+            else if(i==1){
+                cout << "2 ";
+            }
+            else if(i==2){
+                cout << "3 ";
+            }
+        }
+        cout << endl;
+        
     }
-    cout << "Sum is " << sum << endl;
+    
     
     return 0;   
 }
