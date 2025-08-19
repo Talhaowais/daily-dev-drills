c3e6db426210408d1d84bc0c9250bab8bf82d527
M.Talha <talhaowais345@gmail.com>
2025-08-19 10:09:55 +0500

    4th assignment start

---
+++ Assignments/4th_Assignment_while_loop.cpp
@@ -0,0 +1,57 @@
+// all the programs that are done using for loops should be completed using while loop.
+#include <iostream>
+using namespace std;
+int main()
+{
+//fact of a number.
+
+//buzz, fizz
+
+// Q.No.1 Write a program that calculates
+// the sum of even and odd numbers from 1 to N separately
+
+// Q.No.2 Write a program that prints a square of * of size N.
+// Example N = 3
+// ***
+// ***
+// ***
+
+// Q.No.3  Use a for loop to print the following pattern for N rows:
+// Example N=4
+// *
+// **
+// ***
+// ****
+
+// Q.No.4 Write a program that counts how many digits are
+// in a number using a for loop or controlled loop.
+// Example Input: 12345 
+// Output: 5
+
+// num = 123
+// num = num/10 (12)
+// Q.No.5 Write a program using a for loop to 
+// print the first N terms of the Fibonacci series.
+// Example N=5
+// Fibonacci Series
+// 1 1 2 3 5 8 13 21 34 55 
+
+// Q.No.6 Write a program that prints a square of * of size N.
+// Example N = 3
+// ***
+// * *
+// ***
+
+// Q.No.1 Write a program using a for loop to
+//  print numbers from 1 to N (input by user).
+// Q.No.2 Write a program to print all even numbers between 1 and 100 
+// using a for loop.
+// Q.No.3 Write a program to calculate the sum of the first 
+// N natural numbers using a for loop.
+// Q.No.4 Write a program that prints numbers from N to 1 
+// in reverse order using a for loop.
+
+
+
+    return 0;
+}
