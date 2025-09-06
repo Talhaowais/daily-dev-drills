4baf05ae71d61740c9fac3d59761d6261f3f2117
M.Talha <talhaowais345@gmail.com>
2025-09-06 22:00:36 +0500

    6th done now to 7th assignment

--- Assignments/6th_assignment_functions.cpp
+++ Assignments/6th_assignment_functions.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <math.h> 
 using namespace std;
-int maxOfThree(int a, int b, int c);
-// bool isPrime(int n);
-bool isPalindrome(int n);
+//int maxOfThree(int a, int b, int c);
+bool isPrime(int num);
+// bool isPalindrome(int n);
 
 // 1. Maximum of Three Numbers
 
@@ -24,6 +25,7 @@ bool isPalindrome(int n);
 
 int main()
 {
+    //qs1
     // int a,b,c,i;
     // cout << "Enter 1st number: ";
     // cin >> a ;
@@ -32,62 +34,78 @@ int main()
     // cout << "Enter 3rd number: ";
     // cin >> c;
     // cout << "The greatest number is = " << maxOfThree(a,b,c) << endl;
-    //cout << "The prime numbers from 1 - 100 are /n" << isPrime(i);
-    int num;
-    cout << "Sarghoda: " << false << endl;
-    cout << "Enter the number: ";
-    cin >> num;
-    bool check=isPalindrome(num);
-    cout << "Check " << check << endl;
-    return 0;
+
+    //qs2
+    //int i;
+
+    cout << "2 is a prime number.\n";
+    for (int i = 3; i <= 100; i=i+2)
+    {
+       if(isPrime(i)){
+        cout << i << " is a prime number.\n";
+       }
+    }
+    
+
+
+    //qs3
+    // int num;
+    // cout << "Sarghoda: " << false << endl;
+    // cout << "Enter the number: ";
+    // cin >> num;
+    // bool check=isPalindrome(num);
+    // cout << "Check " << check << endl;
+    // return 0;
 }
 // QS 1
-int maxOfThree(int a, int b, int c)
-{
-    if (a > b && a > c)
-    {
-        return a;
-    }
-    else if (b > a && b > c)
-    {
-        return b;
-    }
-    else
-    {
-       return c; 
-    }
-}
-
-// QS 2
-// bool isPrime(int i)
+// int maxOfThree(int a, int b, int c)
 // {
-//     for (int i = 1; i >= 100; i++)
+//     if (a > b && a > c)
 //     {
-//         if (i/i==1)
-//         {
-            
-//         }
-    
+//         return a;
 //     }
-//     return true;
+//     else if (b > a && b > c)
+//     {
+//         return b;
+//     }
+//     else
+//     {
+//        return c; 
+//     }
 // }
 
-//QS 3
-bool isPalindrome(int n)
+// // QS 2
+bool isPrime(int num)
 {
-    int num=n;
-    int  revnum= 0;
-    // using do_while loop
-    do
+   int upperBound=sqrt(num);
+    for (int j = 2; j <= upperBound; j++)
     {
-        cout << "num: " << num << ", revnum: " << revnum << endl;
-        revnum = (revnum*10)+(num%10);
-        num = num/10;
+        if (num%j == 0)
+        {
+            //cout << "Not prime numbers: " << j << endl; 
+            return false;
+        }
+        //cout << "Prime number is: " << j << endl;
         
-    } while (num > 0);
-    if (revnum == n)
-    {
-        return true;
     }
-    return false;
+    return true;
 }
+
+//QS 3
+// bool isPalindrome(int n)
+// {
+//     int num=n;
+//     int  revnum= 0;
+//     // using do_while loop
+//     do
+//     {
+//         cout << "num: " << num << ", revnum: " << revnum << endl;
+//         revnum = (revnum*10)+(num%10);
+//         num = num/10;     
+//     } while (num > 0);
+//     if (revnum == n)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
---
+++ Assignments/7th_Assignment.cpp
@@ -0,0 +1,4 @@
+// 1. Write a function int factorial(int n) that returns the factorial of a given number.
+// 2. Write a function int power(int base, int exp) that calculates base^exp (without using pow from <cmath>).
+// 3. Write a function double celsiusToFahrenheit(double c) that converts Celsius to Fahrenheit.
+// 4. Write a function int calculator(int a, int b, char op) that takes two numbers and an operator (+, -, *, /) and returns the result.
