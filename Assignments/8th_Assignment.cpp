73f6981f935b04a0eefb961829b6b153bb4a9879
M.Talha <talhaowais345@gmail.com>
2025-09-09 10:23:54 +0500

    QS 4 done of 8th assign

--- Assignments/8th_Assignment.cpp
+++ Assignments/8th_Assignment.cpp
@@ -6,3 +6,18 @@
 // Write a program to find the largest and smallest elements in an array.
 // Sum of Elements
 // Write a program to find the sum of all elements in an array.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+//Write a program to find the sum of all elements in an array.
+    int arr[5]={1,2,3,4,5};
+    int c;
+    for(int i=arr[5]; i<=5; i++)
+    {
+        c = i + c;
+    }
+    cout << "The sum of all number in an arry is: " << c << endl;
+    return 0;
+}
