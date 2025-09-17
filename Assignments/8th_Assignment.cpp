8eff21d1e70e08f8eceb96c722546df691c96cff
M.Talha <talhaowais345@gmail.com>
2025-09-17 22:21:20 +0500

    8th asignment done

--- Assignments/8th_Assignment.cpp
+++ Assignments/8th_Assignment.cpp
@@ -69,37 +69,51 @@ int main()
     // cout << "The Largest number is " << max << endl;
 
 // Write a function that takes a string and counts how many vowels and consonants it contains.
-    string str = "talha";
+    string str = "talha!!!!????? SarFaraz";
     cout << "The string is: ";
-    cout << str;
-    int counta = 0, counte = 0, counti = 0, counto = 0, countu = 0, countf, cons;
+    // cin >> str;
+    getline(cin, str); 
+    cout << "String inputted is " << str << endl;
+    int counta = 0, counte = 0, counti = 0, counto = 0, countu = 0, countf, cons, alphCount=0;
+
     for(int j=0; j<=str.length()-1;j++)
     {
-        if (str[j] == 'a')
+        if ((str[j] >= 65 && str[j] <= 90) ||(str[j] >= 97 && str[j] <= 122)) {
+        cout << str[j] << endl;
+            alphCount++;
+        }
+
+        if (str[j] == 'a' || str[j] == 'A')
         {
             counta++;
         }
-        if (str[j] == 'e')
+        if (str[j] == 'e' || str[j] == 'E')
         {
             counte++;
         }
-        if (str[j] == 'i')
+        if (str[j] == 'i' || str[j] == 'I')
         {
             counti++;
         }
-        if (str[j] == 'o')
+        if (str[j] == 'o' || str[j] == 'O')
         {
             counto++;
         }
-        if (str[j] == 'u')
+        if (str[j] == 'u' || str[j] == 'U')
         {
             countu++;
         }  
     }
     countf = counta + counte + counti + counto + countu;
-    cons = str.length() - countf;
+    cons = alphCount - countf;
     cout << "\nThe vowels appeared " << countf << " times in the above string.";
     cout << "\nThe consonants appeared " << cons << " times in the above string." << endl;
+    // A = 65 , Z = 90, a=97, z=122
+    cout << toupper('A') << endl;
+    cout << toupper('Z') << endl;
+    cout << tolower('a') << endl;
+    cout << toupper('z') << endl;
+
     return 0;
 }
 
@@ -118,3 +132,13 @@ int main()
 
     // }
 // }
+
+
+
+
+// Merge Two Arrays
+// Write a program to merge two arrays into one.
+
+
+// Count Words in a Sentence
+// Write a program to count the number of words in a given string.
