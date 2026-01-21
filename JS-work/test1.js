16505edfbea41615bc6f2bb7d00f062c80987d18
Muhammad Talha <talhaowais345@gmail.com>
2026-01-21 21:58:39 +0500

    Conversions

--- JS-work/test1.js
+++ JS-work/test1.js
@@ -1,2 +1,8 @@
 console.log("talha")
 console.log("thx butt")
+let Anumber = "33aaa";
+// let NUmberIsLoggedIn = String(Anumber);
+
+let NUmberIsLoggedIn = Number(Anumber);
+console.log(NUmberIsLoggedIn);
+console.log(typeof(NUmberIsLoggedIn))
