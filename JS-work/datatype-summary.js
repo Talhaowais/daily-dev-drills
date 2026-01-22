87c9161d168a2e1fb6e893088792a872fc71ccec
Muhammad Talha <talhaowais345@gmail.com>
2026-01-22 12:33:48 +0500

    Memory Types

--- JS-work/datatype-summary.js
+++ JS-work/datatype-summary.js
@@ -1,6 +1,6 @@
 // premitive types
 
-//7 types: String, Number, Boolearn ,Null, Undefined, Symbol, BigInt
+//7 types: String, Number, Boolearn ,Null, Undefined, Symbol, BigInt  => uses stack memory
 const number = 2;
 const IsLoggedIn = true;
 const OutSideTemp = null;
@@ -8,12 +8,12 @@ const string= "talha";
 let isemail;
 const id = Symbol('123');
 const anotherid = Symbol('123');
-console.log(id==anotherid); //false
+//console.log(id==anotherid); //false
 const bigNumber = 123456789n;
 
 // JS is a dynamic type language(k humain batana ni prta k type kia ha, wo khud dkhta ha)
 
-// Reference Type, Non-Premitive
+// Reference Type, Non-Premitive => uses heap memory
 
 // Array, Objects, Functions
 
@@ -25,4 +25,26 @@ let myObj = {
 const myFunc = function(){
     console.log("Hello WOrld");
 }
+
+
+//************** Memory Types ******************** */
+
+// Stack (Premitive) & Heap (Non-Premitive)
+// in stack we got a copy but in heap we got a refrence
+let a =3;
+let b = a;
+ a = 4;
+console.log(a);
+console.log(b);
+
+let objone = {
+    name:"talha",
+    age:22
+}
+ let objtwo = objone;
+ objtwo.age = 25;
+console.log(objone.age);
+console.log(objtwo.age);
+
+
 
