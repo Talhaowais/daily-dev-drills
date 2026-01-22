d79382e78bb6875a22d8a173adf30e25522a9773
Muhammad Talha <talhaowais345@gmail.com>
2026-01-22 14:45:25 +0500

    dates

---
+++ JS-work/dates.js
@@ -0,0 +1,21 @@
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.toString());
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleDateString());
+
+console.log(typeof newDate);
+
+let oneDate = new Date(2026, 0, 13)
+// console.log(oneDate);
+
+let twoDate = Date.now();
+console.log(twoDate);
+console.log(oneDate.getTime());
+console.log(Date.now()/1000);
+
+
+
+
+
+
