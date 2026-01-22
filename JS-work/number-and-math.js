c3c13db60955c8bb0acdff47871e3eb85693cbcc
Muhammad Talha <talhaowais345@gmail.com>
2026-01-22 13:18:29 +0500

    Numbers and Math

---
+++ JS-work/number-and-math.js
@@ -0,0 +1,10 @@
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1);
+
+// if a want values between some specific range it becomes
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
+
