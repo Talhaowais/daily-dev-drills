cd63c00c2250df8c40eeed1c35d59d73cae1dfea
Muhammad Talha <talhaowais345@gmail.com>
2026-01-22 11:47:51 +0500

    Operations in js

---
+++ JS-work/operations.js
@@ -0,0 +1,31 @@
+// operations like 2+2,,,,,etc
+
+let val = 4;
+let negval = -val;
+// console.log(negval)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// console.log(2+"2");
+// console.log(2+2+"1");
+
+// console.log("");
+// console.log(true);
+// console.log(false);
+
+// let gamecounter = 100;
+// gamecounter++;
+// console.log(gamecounter);
+
+// prefix and postfix via mdn assignment
+let a = 2;
+let b = a++;
+console.log(`a=${a}, b=${b}`);
+
+let c = 2;
+let d = ++c;
+console.log(`c=${c}, d=${d}`);
