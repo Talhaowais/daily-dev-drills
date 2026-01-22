52b72398955a05b0f10952ca5d3e3cccecb8939a
Muhammad Talha <talhaowais345@gmail.com>
2026-01-22 12:15:00 +0500

    Data Types (Premitive and Reference Type)

---
+++ JS-work/datatype-summary.js
@@ -0,0 +1,28 @@
+// premitive types
+
+//7 types: String, Number, Boolearn ,Null, Undefined, Symbol, BigInt
+const number = 2;
+const IsLoggedIn = true;
+const OutSideTemp = null;
+const string= "talha";
+let isemail;
+const id = Symbol('123');
+const anotherid = Symbol('123');
+console.log(id==anotherid); //false
+const bigNumber = 123456789n;
+
+// JS is a dynamic type language(k humain batana ni prta k type kia ha, wo khud dkhta ha)
+
+// Reference Type, Non-Premitive
+
+// Array, Objects, Functions
+
+const heros = ["abubakar", "umar", "usman", "ali"]
+let myObj = {
+    name: "talha",
+    age: 22
+}
+const myFunc = function(){
+    console.log("Hello WOrld");
+}
+
