d5e75f6970fcec338cf473b9c1daa1910a4b0dee
Muhammad Talha <talhaowais345@gmail.com>
2026-01-24 19:27:30 +0500

    For loop/break nd continue statements

---
+++ JS-work/02-basics_non-premitive(reference)/tables_using_loops.js
@@ -0,0 +1,34 @@
+// tables printing
+for (let i = 0; i <=10; i++) {
+    // console.log(`The table of ${i} is as follow:`);
+    for (let j = 0; j <=10; j++) {
+        // console.log(i + " * " + j + " = " + i*j);   
+    }
+}
+
+// printing all vaues from an array 
+const myarr = ["Qasim", "Adeel", "Zain", "Tahmoor", "Honey" ]
+for(let i=0; i<myarr.length; i++)
+{
+    names = myarr[i]
+    // console.log(names); 
+}
+
+// break and continue
+//break
+for (let index = 1; index <=20; index++) {
+    if(index == 5){
+        console.log(`Required number ${index} is detected.`);
+        break;
+    }
+    console.log(`Value of index is ${index}`); 
+}
+
+//continue
+for (let index = 1; index <=20; index++) {
+    if(index == 5){
+        console.log(`Required number ${index} is detected.`);
+        continue;
+    }
+    console.log(`Value of index is ${index}`); 
+}
