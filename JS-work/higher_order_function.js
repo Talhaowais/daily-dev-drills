39642c24d53e90ff4e7f50cbce1324c32012b988
Muhammad Talha <talhaowais345@gmail.com>
2026-01-25 16:17:19 +0500

    Filters

--- JS-work/higher_order_function.js
+++ JS-work/higher_order_function.js
@@ -48,5 +48,15 @@ const newarr = [
     }
 ]
 newarr.forEach((item)=>{
-    console.log(item.language);
+    // console.log(item.language);
 })
+
+///
+const mynum = [1,2,3,4,5,6,7,8,9]
+const newnum =mynum.filter((num) => num < 4)
+console.log(newnum);
+
+//if we open the scop we have to use the return keyword, but as in the above example, 
+// we dont uses the {}, so its returing.
+
+
