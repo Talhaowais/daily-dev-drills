250cbf452e076ac47ba902cfd2ba86d4e46f6f77
Muhammad Talha <talhaowais345@gmail.com>
2026-01-27 19:05:30 +0500

    Project 2 done

--- JS-work/02-BMI-Calculator-JS-Project/bmi_calculator.js
+++ JS-work/02-BMI-Calculator-JS-Project/bmi_calculator.js
@@ -1,1 +1,30 @@
- 
+const form = document.querySelector('form')
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+    const results2 = document.querySelector('#results2');
+
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = "Please enter a valid height value."
+    }else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = "Please enter a valid weight value."
+    } else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+        results.innerHTML = `<span>${bmi}</span>` 
+
+        if (bmi < 18.6) {
+            results2.innerHTML = "You are under weight"
+        } else if (bmi > 18.6 && bmi < 24.9) {
+            results2.innerHTML = "Your weight is within the normal range"
+        }else if (bmi > 24.9) {
+            results2.innerHTML = "You are over weight"
+        }
+    }
+        
+
+
+});
