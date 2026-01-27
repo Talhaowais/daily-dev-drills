ea949291dfc0bad2ab4097df46c579e6148b2988
Muhammad Talha <talhaowais345@gmail.com>
2026-01-27 18:26:53 +0500

    optimize 1st project

--- JS-work/01-colorChanger-JS-Project/color_changer.js
+++ JS-work/01-colorChanger-JS-Project/color_changer.js
@@ -1,43 +1,53 @@
 const btn = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-btn.forEach(function(btn){
-    console.log(btn);
-    btn.addEventListener('click', function (e) {
-       //console.log(e);
-        //console.log(e.target);
+// btn.forEach(function(btn){
+//     console.log(btn);
+//     btn.addEventListener('click', function (e) {
+//        //console.log(e);
+//         //console.log(e.target);
 
 
-        //by using if statements
-        // if(e.target.id === 'grey'){
-        //     body.style.backgroundColor = e.target.id;
-        // }
-        // if(e.target.id === 'white'){
-        //     body.style.backgroundColor = e.target.id;
-        // }
-        // if(e.target.id === 'blue'){
-        //     body.style.backgroundColor = e.target.id;
-        // }
-        // if(e.target.id === 'yellow'){
-        //     body.style.backgroundColor = e.target.id;
-        // }
+//         //by using if statements
+//         // if(e.target.id === 'grey'){
+//         //     body.style.backgroundColor = e.target.id;
+//         // }
+//         // if(e.target.id === 'white'){
+//         //     body.style.backgroundColor = e.target.id;
+//         // }
+//         // if(e.target.id === 'blue'){
+//         //     body.style.backgroundColor = e.target.id;
+//         // }
+//         // if(e.target.id === 'yellow'){
+//         //     body.style.backgroundColor = e.target.id;
+//         // }
 
 
-        //by using switch statements
-        switch(e.target.id){
-            case 'grey':
-                body.style.backgroundColor = e.target.id;
-                break;
-            case 'white':
-                body.style.backgroundColor = e.target.id;
-                break;
-            case 'blue':
-                body.style.backgroundColor = e.target.id;
-                break;
-            case 'yellow':
-                body.style.backgroundColor = e.target.id;
-        }
+//         //by using switch statements
+//         switch(e.target.id){
+//             case 'grey':
+//                 body.style.backgroundColor = e.target.id;
+//                 break;
+//             case 'white':
+//                 body.style.backgroundColor = e.target.id;
+//                 break;
+//             case 'blue':
+//                 body.style.backgroundColor = e.target.id;
+//                 break;
+//             case 'yellow':
+//                 body.style.backgroundColor = e.target.id;
+//         }   
+
+//        
+
         
-    })
+//     })
 
-});
+// });
+
+ // a pro tip via chatGPT, lets try
+btn.forEach(btn => {
+    btn.addEventListener('click', e => {
+        body.style.backgroundColor = e.target.id;
+    })
+})
