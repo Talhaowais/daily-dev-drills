28e269b8404e04232bf435d919f79cbde4977f8b
Muhammad Talha <talhaowais345@gmail.com>
2026-01-30 16:53:03 +0500

    Project 4 done

---
+++ JS-work/04-Guess-The-Number-JS-Project/guess_the_number.js
@@ -0,0 +1,93 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector('#submit');
+const userInput = document.querySelector('#guessField')
+const guessSlots = document.querySelector('.guesses')
+const remainingSlots = document.querySelector('.lastResult')
+const lowOrHigh = document.querySelector('.lowOrHi')
+const start = document.querySelector('.resultParas')
+
+document.createElement('p');
+let prevGuess = [];
+let  numberGuess = 1;
+
+let playGame = true;
+
+if(playGame){
+    submit.addEventListener('click', function (e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    });
+};
+
+function validateGuess (guess) {
+    if(isNaN(guess)){
+        alert('Please enter a valid number');
+    }else if(isNaN(guess < 1)){
+        alert('Please enter a number greater than 1');
+    }else if(isNaN(guess > 100)){
+        alert('Please enter a number below 100');
+    }else{
+        prevGuess.push(guess);
+        if(numberGuess === 11){
+            displayGuess(guess)
+            displayMessage(`Game Over. Random number was ${randomNumber}`)
+            endGame();
+        }else{
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
+    
+};
+
+function checkGuess (guess) {
+    if(randomNumber === guess){
+        displayMessage('You guessed it right. Congrats')
+        endGame()
+    }else if(randomNumber < guess){
+        displayMessage('Number is Tooo Low')
+    }else if(randomNumber > guess){
+        displayMessage('Number is Tooo High')
+    }
+};
+
+function displayGuess (guess) {
+    userInput.value = '';
+    guessSlots.innerHTML += `${guess}, `
+    numberGuess++;
+    remainingSlots.innerHTML =+ `${11-numberGuess}`
+};
+
+function displayMessage (message) {
+    lowOrHigh.innerHTML = `<h2>${message}</h2>`
+};
+
+function endGame () {
+    userInput.value = '';
+    userInput.setAttribute('disabled', '')
+    p.classList.add('button')
+    p.innerHTML = `<h2 id="newGame">Start New Game</h2>`;
+    start.appendChild(p)
+    playGame = false ;
+    newGame();
+};
+
+function newGame () {
+    const newGameButton = document.querySelector('#newGame');  
+    newGameButton.addEventListener('click', function(){
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numberGuess = 1;
+        guessSlots.innerHTML = '';
+        remainingSlots.innerHTML = `${11 - numberGuess}`;
+        lowOrHigh.innerHTML = '';
+        userInput.removeAttribute('disabled');
+        start.removeChild(p);
+        playGame = true;
+    });
+};
+
+
+
