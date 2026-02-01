4fdf1d0e54dd05c0a2e296449a58ae00675dbb11
Muhammad Talha <talhaowais345@gmail.com>
2026-02-02 00:39:04 +0500

    Done till step 4

--- JS-work/practice_of_map_filter_reduce.js
+++ JS-work/practice_of_map_filter_reduce.js
@@ -41,6 +41,62 @@ console.log(`The largest numer in an array is ${largeNumber}`);
 
 // Step 3:
 
-//
+// Get only the names from the users list.
+const users = [
+  { name: "Ali", age: 22 },
+  { name: "Sara", age: 19 },
+  { name: "Ahmed", age: 25 }
+];
+console.log(users.map((n) => n.name))
 
 
+// Keep users who are 21 or older.
+const people = [
+  { name: "Ayan", age: 16 },
+  { name: "Hassan", age: 21 },
+  { name: "Usman", age: 28 }
+];
+console.log(people.filter((a) => a.age >= 21 ))
+
+// From this list:
+//  Keep products with price greater than 1000
+//  Return only their names
+const products = [
+  { name: "Laptop", price: 80000 },
+  { name: "Mouse", price: 800 },
+  { name: "Keyboard", price: 1500 },
+  { name: "USB Cable", price: 500 }
+];
+console.log(products.filter((p) => p.price > 1000).map((d) => d.name))
+
+
+// Step 4:
+
+// Calculate the total price of all items.
+const cart = [
+  { item: "Book", price: 300 },
+  { item: "Pen", price: 50 },
+  { item: "Bag", price: 1200 }
+];
+let newVal = cart.reduce((acc,c) => acc + c.price, 0)
+console.log(newVal);
+
+// Find the total marks scored by all students.
+const students = [
+  { name: "Ali", marks: 80 },
+  { name: "Sara", marks: 90 },
+  { name: "Ahmed", marks: 70 }
+];
+let newMarks = students.reduce((acc,m) => acc + m.marks, 0)
+console.log(newMarks);
+
+// From this list:
+//  Keep users who are active
+//  Calculate their total balance
+const users2 = [
+  { name: "Ali", active: true, balance: 500 },
+  { name: "Sara", active: false, balance: 700 },
+  { name: "Ahmed", active: true, balance: 300 }
+];
+let finalUser = users2.filter((u2) => u2.active == true).reduce((acc,rd) => acc + rd.balance, 0)
+console.log(finalUser);
