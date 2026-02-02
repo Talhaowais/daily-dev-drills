967840c1e3003b110cf326d258dd100c6b5c1e45
Muhammad Talha <talhaowais345@gmail.com>
2026-02-02 11:19:33 +0500

    1st Question

--- JS-work/2-practice_of_map_filter_reduce.js
+++ JS-work/2-practice_of_map_filter_reduce.js
@@ -1,2 +1,25 @@
 // step 6
 
+// Question 1 — Average (EMPTY ARRAY CASE)
+/* Task:
+Calculate the average score.
+Rules:
+❌ Must NOT return NaN
+❌ Must NOT crash
+✅ If array is empty → return 0 */
+const scores6 = [];
+// let avg = 0;
+// if(scores6.length > 0){
+//     let total = scores6.reduce((acc,num) => acc + num, 0)
+//     avg = total/scores6.length
+// }
+// console.log(avg);
+// we can also wriet the above code as follow
+let avg = scores6.length == 0 ? 0 : scores6.reduce((acc,num) => acc + num, 0)/scores6.length;
+console.log(avg);
+
+
+
+
+
+
