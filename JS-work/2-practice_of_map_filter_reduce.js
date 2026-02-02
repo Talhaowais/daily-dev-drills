f2f0e7fb4f1af03800cd48933edffcb0e96b0a00
Muhammad Talha <talhaowais345@gmail.com>
2026-02-02 18:30:06 +0500

    Start of step 7

--- JS-work/2-practice_of_map_filter_reduce.js
+++ JS-work/2-practice_of_map_filter_reduce.js
@@ -20,6 +20,52 @@ console.log(avg);
 
 
 
+// Question 2 — Total balance (MISSING PROPERTY)
+/*  Task:
+Calculate total balance.
+Rules:
+If balance is missing → treat as 0
+Use reduce only*/
+const users6 = [
+  { name: "Ali", balance: 500 },
+  { name: "Sara" },
+  { name: "Ahmed", balance: 300 }
+];
+let totalBalance = users6.reduce((acc, bal) => acc + (bal.balance || 0), 0)
+console.log(totalBalance);
+
+
+// Question 3 — Find highest (ALL NEGATIVE)
+/* Task:
+Find the highest temperature.
+Rules:
+❌ Do NOT use Math.max
+❌ Do NOT use 0 as initial value
+✅ Must work for all-negative arrays */
+const temps6 = [-10, -20, -5, -30];
+let finaltemp = temps6.reduce((acc,temp) => {
+  if(temp >= acc){
+    acc = temp
+  }
+  return acc;
+},temps6[0])
+console.log(finaltemp);
+
+// Step 7:
+/* Tasks:
+1️⃣ Keep only active users
+2️⃣ Convert into this shape:
+{ name: "Ali", score: logins * 10 }
+3️⃣ Find the highest score */
+const users7 = [
+  { id: 1, name: "Ali", active: true, role: "admin", logins: 10 },
+  { id: 2, name: "Sara", active: false, role: "user", logins: 3 },
+  { id: 3, name: "Ahmed", active: true, role: "user", logins: 7 },
+  { id: 4, name: "Ayesha", active: true, role: "admin", logins: 15 }
+];
+
+
+
 
 
 
