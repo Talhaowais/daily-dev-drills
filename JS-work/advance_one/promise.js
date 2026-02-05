d57d788d95db718324c2d389be316597bbf96374
Muhammad Talha <talhaowais345@gmail.com>
2026-02-05 21:06:12 +0500

    promise

---
+++ JS-work/advance_one/promise.js
@@ -0,0 +1,39 @@
+const { use } = require("react");
+
+// creating a promise and assigning it to a variable    
+const promiseOne = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("1st Async task is complete");
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("1st promise is completed");
+})
+
+
+// 2nd method
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("2nd Async task is complete");
+        resolve()
+    }, 1000);
+}).then(function(){
+    console.log("2nd promise is completed");
+})
+
+
+// Creating 3rd promise
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("3rd Async task is complete");
+        // In resolve we can pass either object, array or function as we like 
+        resolve({username: "Talha", email: "talhaowais345@gmail.com"})
+    }, 1000);
+})
+promiseThree.then(function(user){
+    console.log(user);
+    console.log("3rd promise is completed");
+
+})
