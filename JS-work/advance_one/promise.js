ee587f1b4071e3a58ede9cf128565b030ee18b12
Muhammad Talha <talhaowais345@gmail.com>
2026-02-07 17:39:40 +0500

    Promise Adition

--- JS-work/advance_one/promise.js
+++ JS-work/advance_one/promise.js
@@ -26,13 +26,34 @@ new Promise(function(resolve, reject){
 // Creating 3rd promise
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log("3rd Async task is complete");
+        // console.log("3rd Async task is complete");
         // In resolve we can pass either object, array or function as we like 
         resolve({username: "Talha", email: "talhaowais345@gmail.com"})
     }, 1000);
 })
 promiseThree.then(function(user){
     console.log(user);
-    console.log("3rd promise is completed");
+    // console.log("3rd promise is completed");
 
+})
+
+// The use of reject
+const fourthPromise = Promise((function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "Talha-2", email: "2nd-talhaowais345@gmail.com"})
+        }else{
+            reject("Error:You're missing something")
+        }
+    }, 1000)
+}))
+fourthPromise.then((user) => {
+    console.log(user);
+    return user.username
+}).then(() => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+    
 })
