62b2f1dd58890b50ce6d73b82cb6d8f74b83dbff
Muhammad Talha <talhaowais345@gmail.com>
2026-02-13 11:24:16 +0500

    Creating Custom library

---
+++ React/Custom-React/custom_react.js
@@ -0,0 +1,14 @@
+
+function customRender(reactElement, container){
+    const domElemet = document.createElement 
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+const mainContainer = document.querySelector('#root')
