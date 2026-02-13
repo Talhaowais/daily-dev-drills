d250efacf6d5ace48a15df9af6b44339f4d02e3e
Muhammad Talha <talhaowais345@gmail.com>
2026-02-13 21:33:09 +0500

    Start of Count project

---
+++ React/02Counter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is a Counter project</h1>
+      <h3>Count Is: 15</h3>
+      <button>Increase Count = </button>
+      <br />
+      <button>Decrease Count = </button>
+      
+    </>
+  )
+}
+
+export default App
