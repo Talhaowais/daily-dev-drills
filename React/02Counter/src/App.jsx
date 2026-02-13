43a60f24389bc564e7cff8d41bc1e1fd368e6bc0
Muhammad Talha <talhaowais345@gmail.com>
2026-02-13 22:18:34 +0500

    Pangay hehehhe

--- React/02Counter/src/App.jsx
+++ React/02Counter/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -5,12 +6,15 @@ import './App.css'
 
 function App() {
   let [counter, setcounter] = useState(15)
+  let [message, setmessage] = useState("")
   const addVal = () => {
     console.log("Clicked", counter);
     // counter = counter + 1;
     setcounter(counter + 1)
     if (counter >= 20) {
       console.log("The value reaches 20 so again reset to 15");
+      const element = React.createElement("h3", null, "The value reaches 20 so again reset to 15")
+      setmessage("Value reaches to 20 → reset again to 15")
       setcounter(15)
     }
   }
@@ -20,7 +24,8 @@ function App() {
     setcounter(counter - 1)
     if (counter <= 0) {
       console.log("The value reaches zero so again reset to 15");
-      
+      const element = React.createElement("h3", null, "The value reaches zero so again reset to 15")
+      setmessage("Value reaches to 0 → reset again to 15")
       setcounter(15)
     }
   }
@@ -30,8 +35,10 @@ function App() {
       <h1>This is a Counter project</h1>
       <h3>Count Is: {counter}</h3>
       <button onClick={addVal}>Increase Count: {counter}</button>
+      <h3>{message}</h3>
       <br />
       <button onClick={delVal}>Decrease Count: {counter}</button>
+      <h3>{message}</h3>
       
     </>
   )
