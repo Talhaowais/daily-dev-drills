ef6ee49459a1a4537c5aefb366a3ecab83099b1d
Muhammad Talha <talhaowais345@gmail.com>
2026-02-13 12:19:43 +0500

    Created Custom React/ Learn how the react works in tree

--- React/Custom-React/custom_react.js
+++ React/Custom-React/custom_react.js
@@ -1,6 +1,20 @@
 
 function customRender(reactElement, container){
-    const domElemet = document.createElement 
+    // Version 1 of code:
+    // const domElemet = document.createElement(reactElement.type)
+    // domElemet.innerHTML = reactElement.children
+    // domElemet.setAttribute('href', reactElement.props.href)
+    // domElemet.setAttribute('target', reactElement.props.target)
+    // container.appendChild(domElemet)
+
+    // Version 2 of code:
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
+    container.appendChild(domElement)
 }
 
 const reactElement = {
@@ -12,3 +26,4 @@ const reactElement = {
     children: 'Click me to visit google'
 }
 const mainContainer = document.querySelector('#root')
+customRender(reactElement, mainContainer)
