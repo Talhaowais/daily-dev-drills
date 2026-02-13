9be75eecabf582fa7a2dc36cf59a865d512f9345
Muhammad Talha <talhaowais345@gmail.com>
2026-02-13 22:02:18 +0500

    Counter Program Done

--- React/02Counter/src/App.jsx
+++ React/02Counter/src/App.jsx
@@ -4,14 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let [counter, setcounter] = useState(15)
+  const addVal = () => {
+    console.log("Clicked", counter);
+    // counter = counter + 1;
+    setcounter(counter + 1)
+    if (counter >= 20) {
+      console.log("The value reaches 20 so again reset to 15");
+      setcounter(15)
+    }
+  }
+  const delVal = () => {
+    console.log("Clicked", counter);
+    // counter = counter - 1;
+    setcounter(counter - 1)
+    if (counter <= 0) {
+      console.log("The value reaches zero so again reset to 15");
+      
+      setcounter(15)
+    }
+  }
 
   return (
     <>
       <h1>This is a Counter project</h1>
-      <h3>Count Is: 15</h3>
-      <button>Increase Count = </button>
+      <h3>Count Is: {counter}</h3>
+      <button onClick={addVal}>Increase Count: {counter}</button>
       <br />
-      <button>Decrease Count = </button>
+      <button onClick={delVal}>Decrease Count: {counter}</button>
       
     </>
   )
