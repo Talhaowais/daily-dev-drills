cbaeaaf67de11dad6b2338a99d296faa77d5e4a1
Muhammad Talha <talhaowais345@gmail.com>
2026-02-24 21:22:15 +0500

    minor fixes

--- React/08_Scratch_WebSite/src/App.jsx
+++ React/08_Scratch_WebSite/src/App.jsx
@@ -4,12 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [home, setHome] = useState("Home")
+  const [counter, setCounter] = useState("Counte")
+  const [about, setAbout] = useState("About Us")
+  const [info, setInfo] = useState("Info")
+
 
   return (
-    <>
-     <h1>A dummy website template</h1>
-    </>
+    <div className='h-screen w-screen bg-center bg-cover bg-no-repeat transition-all duration-500' style={{backgroundImage: img}}>
+      <div className='fixed flex flex-wrap justify-center top-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/34710886/pexels-photo-34710886.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Red"}}>Home
+          </button>
+          
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/35715509/pexels-photo-35715509.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Green"}}>Counter
+          </button>
+          
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/35846889/pexels-photo-35846889.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Blue"}}>About US
+          </button>
+          
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/36069169/pexels-photo-36069169.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Orange"}}>Info
+          </button>
+          
+        </div>
+      </div>
+    </div>
+    
   )
 }
-
 export default App
---
+++ React/08_Scratch_WebSite/src/components/Index.js
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import Count from "./Count";
+import AboutUs from "./AboutUs";
+import Info from "./Info";
+
+export {Home, Count, AboutUs, Info}
