93c3df0ae126d4ab0b5aa677361daa0366ddfa48
Muhammad Talha <talhaowais345@gmail.com>
2026-02-24 16:20:56 +0500

    Done Img Changer

--- React/07_Image_Changer/src/App.jsx
+++ React/07_Image_Changer/src/App.jsx
@@ -4,32 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [img, setImg] = useState("url(https://images.pexels.com/photos/36027809/pexels-photo-36027809.jpeg)")
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='h-screen w-screen bg-center bg-cover bg-no-repeat transition-all duration-500' style={{backgroundImage: img}}>
+      <div className='fixed flex flex-wrap justify-center top-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/34710886/pexels-photo-34710886.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Red"}}>Flower
+          </button>
+          
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/35715509/pexels-photo-35715509.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Green"}}>Gallery
+          </button>
+          
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/35846889/pexels-photo-35846889.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Blue"}}>Mountain
+          </button>
+          
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/36069169/pexels-photo-36069169.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Orange"}}>See
+          </button>
+          
+          <button 
+          onClick={() => {setImg("url(https://images.pexels.com/photos/15524988/pexels-photo-15524988.jpeg)")}}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-2xl cursor-pointer' style={{backgroundColor: "Olive"}}>Evening
+          </button>
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+    
   )
 }
-
 export default App
